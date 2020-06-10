ff342332952bd179fa090dd4ae8f1a084929c9cc
Matt Wade <matt.wade@designory.com>
2020-06-10 16:28:13 -0500

    Allows for custom image selectors on non-image hovers

--- src/hover-image.js
+++ src/hover-image.js
@@ -3,6 +3,7 @@ import delegate from 'delegate';
 /**
  * @param {String} [options.hoverSrcAttribute] The data attribute the library will attach its delegated events to.
  * @param {String} [options.orginalSrcAttribute] The name of the data attribute the library will save the original source URL while the image is swapped out.
+ * @param {String} [options.hoverImageSelectorAttribute] When the `hoverSrcAttribute` is placed on a non-image element, this optional attribute allows for a selector to be passed for the child image that'll be swapped out. When not this attribute is not present, its value defaults to 'img'.
  * @param {String} [options.classToggle] The class that will get toggled while the image is swapped out on hover.
  * @param {Boolean} [options.preloadHoverImages] When true, will make a network request for all images specified within the `hoverSrcAttribute` before the initial `mouseover` event has fired.
  * @returns {Function} Returns a method to destroy the event listeners we placed.
@@ -10,6 +11,7 @@ import delegate from 'delegate';
 const initializeHoverImage = ({
 	hoverSrcAttribute = 'data-hover-src',
 	orginalSrcAttribute = 'data-original-src',
+	hoverImageSelectorAttribute = 'data-image-selector',
 	classToggle = 'is-hovered',
 	preloadHoverImages = true,
 } = {}) => {
@@ -25,7 +27,13 @@ const initializeHoverImage = ({
 		let image = target;
 
 		if (image.tagName !== 'IMG') {
-			image = target.querySelector('img');
+			// Defaults to looking for the first `<img>` tag in querySelector's depth-first search
+			let image_selector = 'img';
+			if (target.hasAttribute(hoverImageSelectorAttribute)) {
+				image_selector = target.getAttribute(hoverImageSelectorAttribute);
+			}
+
+			image = target.querySelector(image_selector);
 			if (!image) {
 				return;
 			}
@@ -59,7 +67,12 @@ const initializeHoverImage = ({
 		let image = target;
 
 		if (image.tagName !== 'IMG') {
-			image = target.querySelector('img');
+			let image_selector = 'img';
+			if (target.hasAttribute(hoverImageSelectorAttribute)) {
+				image_selector = target.getAttribute(hoverImageSelectorAttribute);
+			}
+
+			image = target.querySelector(image_selector);
 			if (!image) {
 				return;
 			}
